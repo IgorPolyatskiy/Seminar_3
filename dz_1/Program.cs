b68f320124f9be576c86068254c2af5cff9e04c7
IgorPolyatskiy <polyatsky.igor@yandex.ru>
2023-01-11 15:02:23 +0700

    Добавил ДЗ(Семинар 3)

---
+++ dz_1/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+int[] numberAsArray = new int[5];
+
+for (int i = 0; i < 5; i++)
+{
+    numberAsArray[i] = number % 10;
+    number = number / 10;
+}
+if (numberAsArray[0] == numberAsArray[4] && numberAsArray[1] == numberAsArray[3])
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
---
+++ dz_3/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число: ");
+string input = Console.ReadLine();
+int number = int.Parse(input);
+
+for (int i = 1; i <= number; i++)
+{
+    int result = i * i * i;
+    Console.Write(result);
+    if (i < number)
+    {
+        Console.Write(", ");
+    }
+}
